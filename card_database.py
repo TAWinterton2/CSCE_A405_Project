8913fc6c7829928be34520a13d57383ececdb679
TAWinterton2 <tawinterton2@alaska.edu>
2023-11-17 01:44:41 -0500

    Finished Implemnting Class templates and Attack Action Cards

---
+++ card_database.py
@@ -0,0 +1,37 @@
+import card_template as card
+
+
+#Whirling Mist Blossom
+Whirling_Mist_Blossom = card.NinjaAttackAction("Whirling Mist Blossom", 1, 2, 2, True)
+
+#Salt The Wound
+Salt_The_Wound = card.NinjaAttackAction("Salt the Wound", 0, 2, 3, False)
+
+#Bittering Thorns
+Bittering_Thorns = card.NinjaAttackAction("Bittering Thorns", 1, 3, 2, True)
+
+#Torrent of Tempo
+Torrent_of_Tempo = card.NinjaAttackAction("Torrent of Tempo", 1, 5, 3, False)
+
+#Flying Kick
+Flying_Kick = card.NinjaAttackAction("Flying Kick", 2, 5, 3, False)
+
+#Head Jab
+Head_Jab = card.NinjaAttackAction("Head Jab", 0, 1, 2, True )
+
+#Brutal assault
+Brutal_Assult = card.GenericAttackAction("Brutal Assault", 3, 4, 3, False)
+
+#Scar for a Scar
+Scar_for_a_Scar = card.GenericAttackAction("Scar for a Scar", 0, 4, 2, False)
+
+
+
+
+
+
+
+
+
+
+
--- card_template.py
+++ card_template.py
@@ -2,27 +2,61 @@
 class Card:
     def __init__(self, cardName):
         self.cardName = cardName
-        self.cardType = None
         self.description = None
-        self.cardClass = None
-        self.cardBlock = None
-        self.cardAttack = None
-        self.cardDiscript = None
+        self.type = None
+        
 
+    # Debugging Functions
     def printname(self):
         print(self.cardName)
+    def printDescription(self):
+        print(self.description)
 
 
-class GenericActionCard(Card):
-    def __init__(self, cardName, block):
+#Generic Card Template
+
+class GenericAction(Card):
+    def __init__(self, cardName, pitch, block, goAgian):
         super().__init__(cardName)
-        self.cardType = 'Action'
-        self.cardClass = "Generic"
+        self.block = block
+        self.pitch = pitch 
+        self.goagian = bool(goAgian)
+        self.type = "Generic Action"
 
-class GenericAttackActionCard(GenericActionCard):
-    def __init__(self, cardName):
-        super().__init__(cardName)
-        self.cardtype = "Action Card - Attack"
+
+    # Debugging Functions
+    def printblock(self):
+        print(self.block)
+
+    def printgoAgain(self):
+        print(self.goagian)
+    
+    def printPitch(self):
+        print(self.pitch)
+
+        
+
+class GenericAttackAction(GenericAction):
+    def __init__(self, cardName, pitch, attack, block, goAgian):
+        super().__init__(cardName, pitch, block, goAgian)
+        self.attack = attack
+        self.description = '"By the all-seeing eye, I spill this blood in your \n image, I sacrifice this foul beast unto you, \n that you might take- Wait. \n What are you doing? Dont- no!" - Septus'
+        self.type = "Generic Action - Attack"
+    def printAttack(self):
+        print(self.attack)
+
+
+#Ninja Card Template
+class NinjaAttackAction(GenericAttackAction):
+    def __init__(self, cardName, pitch, attack, block, goAgian):
+        super().__init__(cardName, pitch, attack, block, goAgian)
+        self.type = "Ninja Action - Attack"
+
+
+        
+
+
+
 
 
 
