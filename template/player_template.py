6ba16e47bf0cac6ca7308cb6f448add05ab24e63
TAWinterton2 <tawinterton2@alaska.edu>
2023-12-06 18:13:22 -0500

    updated player and added gamestate.py

--- template/player_template.py
+++ template/player_template.py
@@ -5,6 +5,9 @@ class player():
     def __init__(self):
         self.hand = []
         self.handsize = 4
+        self.pitchzone = []
+        self.resources = 0
+        self.playedcards = []
         print("Player has joined the game\n ")
     
     def draw(self, deck):
@@ -13,6 +16,19 @@ class player():
         print("New hand drawn: ")
         return self
     
+    def pitch(self, cardname):
+        index = cardname
+        try:
+            self.resources += index.pitch
+            self.pitchzone.append(self.hand.pop(index))
+
+        except:
+            print("Error trying to pitch card")
+    
     def showHand(self):
         for card in self.hand:
+            card.show()
+
+    def showPitch(self):
+        for card in self.pitchzone:
             card.show()
